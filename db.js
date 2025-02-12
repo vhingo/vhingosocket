22133dfa4b3eb577b177ad356733e69e0106d5f3
hitendra <hitendra@technoscore.net>
2025-02-12 16:21:59 +0530

    add the .env file

--- db.js
+++ db.js
@@ -1,12 +1,15 @@
+import dotenv from 'dotenv';
 import mysql from 'mysql';
 
+dotenv.config();
+
 const db = mysql.createConnection({
-  host: '93.127.185.122',
-  port: 3306,
-  user: 'vhingo_socket2',
-  password: 'cfKoRBzn0fTw',
-  database: 'vhingo_db',
-  connectTimeout: 20000
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  connectTimeout: 20000,
 });
 
 db.connect((err) => {
